67fc9a6e3231f25b1d63e4d98359647eb237253a
Kevin <kevincornejoandrade@kevins-MacBook-Pro-2.local>
2017-03-09 17:48:10 -0500

    updated remote db URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
   "URI": "mongodb://localhost/phonebook"
-  //Remote MongoDB deployment -> let URI = "mongodb://kev:qweasd@ds054999.mlab.com:54999/phonebook-kevin";
+  //Remote MongoDB deployment -> let URI = "mongodb://kev:qweasd@ds040089.mlab.com:40089/phonebook";
 };
